68341aa3294fbc5ec2c6dc02d5a125d3979af6ce
Makar0k <57058885+Makar0k@users.noreply.github.com>
2022-07-17 15:57:24 +0300

    Assets refactoring and minor AI update

---
+++ New Unity Project/Assets/INavLinkHandler.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface INavLinkHandler
+{
+    public abstract void LinkHandleStart();
+    public abstract void LinkHandleEnd();
+}
